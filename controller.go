934fba56996d4a1f173ab7ebedf0c57195210d3e
utsab818 <lolgang08@gmail.com>
2024-06-01 15:34:26 +0530

    Updated ingress resource

--- controller.go
+++ controller.go
@@ -7,6 +7,7 @@ import (
 
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
+	netv1 "k8s.io/api/networking/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/wait"
 	appinformers "k8s.io/client-go/informers/apps/v1"
@@ -116,11 +117,11 @@ func (c *controller) syncDeployment(ns, name string) error {
 	// our deployment's container is listening on
 	svc := corev1.Service{
 		ObjectMeta: metav1.ObjectMeta{
-			Name: dep.Name,
+			Name:      dep.Name,
 			Namespace: ns,
 		},
 		Spec: corev1.ServiceSpec{
-	        // get label for deployment to use as selector is service
+			// get label for deployment to use as selector is service
 			Selector: depLabels(*dep),
 			Ports: []corev1.ServicePort{
 				{
@@ -130,15 +131,55 @@ func (c *controller) syncDeployment(ns, name string) error {
 			},
 		},
 	}
-	_, err = c.clientset.CoreV1().Services(ns).Create(ctx, &svc, metav1.CreateOptions{})
+	s, err := c.clientset.CoreV1().Services(ns).Create(ctx, &svc, metav1.CreateOptions{})
 	if err != nil {
 		fmt.Printf("error %s, creating service\n", err.Error())
 	}
 	// create ingress
-	return nil
+	return createIngress(ctx, c.clientset, s)
 }
 
-func depLabels(dep appsv1.Deployment) map[string]string{
+func createIngress(ctx context.Context, client kubernetes.Interface, svc *corev1.Service) error {
+	pathType := "Prefix"
+	ingress := netv1.Ingress{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      svc.Name,
+			Namespace: svc.Namespace,
+			Annotations: map[string]string{
+				"nginx.ingress.kubernetes.io/rewrite-target": "/",
+			},
+		},
+		Spec: netv1.IngressSpec{
+			Rules: []netv1.IngressRule{
+				netv1.IngressRule{
+					IngressRuleValue: netv1.IngressRuleValue{
+						HTTP: &netv1.HTTPIngressRuleValue{
+							Paths: []netv1.HTTPIngressPath{
+								netv1.HTTPIngressPath{
+									Path:     fmt.Sprintf("/%s", svc.Name),
+									PathType: (*netv1.PathType)(&pathType),
+									Backend: netv1.IngressBackend{
+										Service: &netv1.IngressServiceBackend{
+											Name: svc.Name,
+											Port: netv1.ServiceBackendPort{
+												Number: 80,
+											},
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	_, err := client.NetworkingV1().Ingresses(svc.Namespace).Create(ctx, &ingress, metav1.CreateOptions{})
+	return err
+}
+
+func depLabels(dep appsv1.Deployment) map[string]string {
 	return dep.Spec.Template.Labels
 }
 
